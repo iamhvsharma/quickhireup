b51d9b4d5fedc16714c46ca0b07ed5db395f0ed4
Mk016 <mahendrakumawat80224@gmail.com>
2024-12-12 04:37:55 +0530

    connect the bacend and update frontend ui

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,11 +1,25 @@
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'http://localhost:8000', // Your backend server URL
-    timeout: 5000,
-    headers: {
-        'Accept': 'application/json'
-    }
+const instance = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
 });
 
-export default api;
+// Add basic error logging
+instance.interceptors.request.use(request => {
+  console.log('Starting Request', request);
+  return request;
+});
+
+instance.interceptors.response.use(response => {
+  console.log('Response:', response);
+  return response;
+}, error => {
+  console.error('API Error:', error.message);
+  return Promise.reject(error);
+});
+
+export default instance;
