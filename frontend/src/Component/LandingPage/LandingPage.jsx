79d03e3d1d7e89c874b8fd0cdfd1574d9aecb2d5
Mk016 <mahendrakumawat80224@gmail.com>
2024-12-12 04:55:32 +0530

    remove the modifed files

--- frontend/src/Component/LandingPage/LandingPage.jsx
+++ frontend/src/Component/LandingPage/LandingPage.jsx
@@ -3,18 +3,19 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Features from "./Features";
 import Question from "./Questions";
-
 import DiscoverJob from './DiscoverJob'
 import Footer from "./Footer";
 import Logo from './CompanyLogos'
-import WhyChooseUs from './WhyChooseUs'
-// import InfiniteScrollCard from "./InfiniteScrollCard";
 import { AnimatedShinyTextDemo } from "./AnimatedText";
+
 function LandingPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center relative">
       {/* Background pattern and gradient */}
-      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]">
+        </div>
+      </div>
 
       {/* Content */}
       <div className="w-full max-w-7xl space-y-10">
@@ -31,4 +32,4 @@ function LandingPage() {
   );
 }
 
-export default LandingPage;
+export default LandingPage;
